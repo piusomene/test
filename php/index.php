eee49040c05193f42aad72f5249f29d87553bd4f
piusomene <piusomene@gmail.com>
2024-09-02 04:58:23 -0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,257 +2,97 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Themis - Pius Omene | Law Business Template</title>
-    <meta name="keywords" content="lawyer, law, family law, immigration law, real estate, wills, estates, probate, corporate law, litigation">
-    <meta name="description" content="Pius Omene is a professional lawyer based in Penticton, BC, offering legal services in Family Law, Canadian Immigration Law, Real Estate, Wills and Estates, Probate, Corporate Law, and General Litigation.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="style/jquery.qtip.css">
-    <link rel="stylesheet" href="style/jquery.popeye.css">
-    <link rel="stylesheet" href="style/jquery.jScrollPane.css">
-    <link rel="stylesheet" href="style/base.css">
-    <link rel="stylesheet" href="style/page.css">
-    <link href="https://fonts.googleapis.com/css?family=Voces" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Noticia+Text:400,400italic,700,700italic" rel="stylesheet">
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
 
-    <!-- JavaScript Files -->
-    <script src="script/jquery.min.js"></script>
-    <script src="script/jquery.easing.js"></script>
-    <script src="script/jquery.blockUI.js"></script>
-    <script src="script/jquery.bxSlider.js"></script>
-    <script src="script/jquery.qtip.min.js"></script>
-    <script src="script/jquery.popeye.min.js"></script>
-    <script src="script/jquery.ba-bqq.min.js"></script>
-    <script src="script/jquery.mousewheel.js"></script>
-    <script src="script/jquery.jScrollPane.js"></script>
-    <script src="script/script.js"></script>
-    <script src="script/themis.js"></script>
-    <script src="script/main.js"></script>
+        header {
+            background: #35424a;
+            color: #ffffff;
+            padding-top: 30px;
+            min-height: 70px;
+            border-bottom: #e8491d 3px solid;
+        }
+
+        header a {
+            color: #ffffff;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-size: 16px;
+        }
+
+        ul {
+            padding: 0;
+            list-style: none;
+        }
+
+        ul li {
+            float: left;
+            display: inline;
+            padding: 0 20px 0 20px;
+        }
+
+        .footer {
+            background: #35424a;
+            color: #ffffff;
+            text-align: center;
+            padding: 30px 0;
+            margin-top: 30px;
+        }
+
+        .footer a {
+            color: #ffffff;
+            text-decoration: none;
+            font-size: 14px;
+        }
+    </style>
 </head>
 <body>
-    <!-- Main Container -->
-    <div class="main box-center">
-        <!-- Header Section -->
-        <div class="header overflow-fix">
-            <div class="layout-7030 overflow-fix">
-                <!-- Left Column -->
-                <div class="layout-7030-left overflow-fix">
-                    <!-- Logo -->
-                    <div class="header-logo">
-                        <a href="#!main">
-                            <span class="title">Pius Omene</span>
-                            <span class="subtitle">Private Lawyer</span>
-                        </a>
-                    </div>
-                    <!-- Quote -->
-                    <div class="header-quote">
-                        <h5>Realize your legal rights</h5>
-                        <h5><span>with qualified help</span></h5>
-                    </div>
-                </div>
-                <!-- Right Column -->
-                <div class="layout-7030-right">
-                    <!-- Phone Number -->
-                    <div class="header-phone">
-                        <h5>Phone</h5>
-                        <h5><span>778-760-3751</span></h5>
-                    </div>
-                </div>
+
+    <header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">My</span> Website</h1>
             </div>
-        </div>
-        <!-- Content Section -->
-        <div class="content">
-            <div id="themis">
-                <!-- Vertical Menu -->
-                <ul id="themis-vertical-menu">
-                    <!-- About -->
-                    <li class="themis-vertical-menu-box-1">
-                        <div class="themis-vertical-menu-box-wrapper">
-                            <div class="themis-vertical-menu-box-background"></div>
-                            <div class="themis-vertical-menu-box-foreground"></div>
-                            <div class="themis-vertical-menu-box-foreground-hover"></div>
-                            <div class="themis-vertical-menu-box-icon"></div>
-                            <div class="themis-vertical-menu-box-icon-hover"></div>
-                            <h3>About</h3>
-                            <h6>Pius Omene</h6>
-                            <a href="#!about"></a>
-                        </div>
-                    </li>
-                    <!-- Services -->
-                    <li class="themis-vertical-menu-box-2">
-                        <div class="themis-vertical-menu-box-wrapper">
-                            <div class="themis-vertical-menu-box-background"></div>
-                            <div class="themis-vertical-menu-box-foreground"></div>
-                            <div class="themis-vertical-menu-box-foreground-hover"></div>
-                            <div class="themis-vertical-menu-box-icon"></div>
-                            <div class="themis-vertical-menu-box-icon-hover"></div>
-                            <h3>Services</h3>
-                            <h6>Legal Services</h6>
-                            <a href="#!services"></a>
-                        </div>
-                    </li>
-                    <!-- Clients -->
-                    <li class="themis-vertical-menu-box-3">
-                        <div class="themis-vertical-menu-box-wrapper">
-                            <div class="themis-vertical-menu-box-background"></div>
-                            <div class="themis-vertical-menu-box-foreground"></div>
-                            <div class="themis-vertical-menu-box-foreground-hover"></div>
-                            <div class="themis-vertical-menu-box-icon"></div>
-                            <div class="themis-vertical-menu-box-icon-hover"></div>
-                            <h3>Clients</h3>
-                            <h6>Testimonials</h6>
-                            <a href="#!clients"></a>
-                        </div>
-                    </li>
-                    <!-- Practice -->
-                    <li class="themis-vertical-menu-box-4">
-                        <div class="themis-vertical-menu-box-wrapper">
-                            <div class="themis-vertical-menu-box-background"></div>
-                            <div class="themis-vertical-menu-box-foreground"></div>
-                            <div class="themis-vertical-menu-box-foreground-hover"></div>
-                            <div class="themis-vertical-menu-box-icon"></div>
-                            <div class="themis-vertical-menu-box-icon-hover"></div>
-                            <h3>Practice</h3>
-                            <h6>Areas of Practice</h6>
-                            <a href="#!practice"></a>
-                        </div>
-                    </li>
-                    <!-- Contact -->
-                    <li class="themis-vertical-menu-box-5">
-                        <div class="themis-vertical-menu-box-wrapper">
-                            <div class="themis-vertical-menu-box-background"></div>
-                            <div class="themis-vertical-menu-box-foreground"></div>
-                            <div class="themis-vertical-menu-box-foreground-hover"></div>
-                            <div class="themis-vertical-menu-box-icon"></div>
-                            <div class="themis-vertical-menu-box-icon-hover"></div>
-                            <h3>Contact</h3>
-                            <h6>Get In Touch</h6>
-                            <a href="#!contact"></a>
-                        </div>
-                    </li>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="contact.html">Contact</a></li>
                 </ul>
-                <!-- Window Section -->
-                <div id="themis-window">
-                    <a href="#!main" id="themis-close-button"></a>
-                    <div id="themis-window-scroll">
-                        <div id="themis-window-content">
-                            <!-- Dynamic Content Goes Here -->
-                        </div>
-                    </div>
-                    <!-- Horizontal Menu -->
-                    <div id="themis-horizontal-menu-wrapper">
-                        <ul id="themis-horizontal-menu">
-                            <!-- About -->
-                            <li class="themis-horizontal-menu-box-1">
-                                <a href="#!about">
-                                    <span class="themis-horizontal-menu-icon"></span>
-                                    <span class="themis-horizontal-menu-icon-hover"></span>
-                                    <span class="title">About</span>
-                                    <span class="subtitle">Pius Omene</span>
-                                </a>
-                            </li>
-                            <!-- Services -->
-                            <li class="themis-horizontal-menu-box-2">
-                                <a href="#!services">
-                                    <span class="themis-horizontal-menu-icon"></span>
-                                    <span class="themis-horizontal-menu-icon-hover"></span>
-                                    <span class="title">Services</span>
-                                    <span class="subtitle">Legal Services</span>
-                                </a>
-                            </li>
-                            <!-- Clients -->
-                            <li class="themis-horizontal-menu-box-3">
-                                <a href="#!clients">
-                                    <span class="themis-horizontal-menu-icon"></span>
-                                    <span class="themis-horizontal-menu-icon-hover"></span>
-                                    <span class="title">Clients</span>
-                                    <span class="subtitle">Testimonials</span>
-                                </a>
-                            </li>
-                            <!-- Practice -->
-                            <li class="themis-horizontal-menu-box-4">
-                                <a href="#!practice">
-                                    <span class="themis-horizontal-menu-icon"></span>
-                                    <span class="themis-horizontal-menu-icon-hover"></span>
-                                    <span class="title">Practice</span>
-                                    <span class="subtitle">Areas of Practice</span>
-                                </a>
-                            </li>
-                            <!-- Contact -->
-                            <li class="themis-horizontal-menu-box-5">
-                                <a href="#!contact">
-                                    <span class="themis-horizontal-menu-icon"></span>
-                                    <span class="themis-horizontal-menu-icon-hover"></span>
-                                    <span class="title">Contact</span>
-                                    <span class="subtitle">Get In Touch</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            </nav>
         </div>
-    </div>
+    </header>
 
-    <!-- Custom Footer Section -->
-    <div class="custom-footer">
-        <div class="contact-info">
-            <h2>Contact Information</h2>
-            <p><strong>Address:</strong> 129 Nanaimo Ave W, Penticton, BC V2A 1N2</p>
-            <p><strong>Phone:</strong> 778-760-3751</p>
-            <p><strong>Email:</strong> <a href="mailto:pius@piusomene.com">pius@piusomene.com</a></p>
+    <section id="main">
+        <div class="container">
+            <h1>Welcome to My Website</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         </div>
-        <div class="call-to-action">
-            <h2>Book a Consultation</h2>
-            <p>Ready to discuss your legal needs? <a href="#!contact" class="cta-button">Book Now</a></p>
-        </div>
-    </div>
+    </section>
 
-    <!-- Footer Bottom -->
-    <div class="footer-bottom">
-        <hr>
-        <div class="main box-center">
-            <div class="overflow-fix clear-fix layout-50">
-                <div class="layout-50-left">
-                    <a href="#">Copyright © 2024 Pius Omene</a>
-                </div>
-                <div class="layout-50-right">
-                    <ul class="no-list footer-bottom-menu">
-                        <li><a href="#!about">About</a></li>
-                        <li><a href="#!services">Services</a></li>
-                        <li><a href="#!clients">Clients</a></li>
-                        <li><a href="#!practice">Practice</a></li>
-                        <li><a href="#!contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+    <footer class="footer">
+        <p>Address: 1234 Street Name, City, State, Zip</p>
+        <p>Phone: (123) 456-7890</p>
+        <p>Email: info@example.com</p>
+        <p>Areas of Practice: Family Law, Criminal Law, Business Law</p>
+        <p>&copy; 2023 Your Law Firm Name. All rights reserved.</p>
+    </footer>
 
-    <!-- Initialization Script -->
-    <script>
-        var page = {
-            "about": {"js":"page-1.js","html":"page-1.html","title":"Themis - Pius Omene | About"},
-            "services": {"js":"page-2.js","html":"page-2.html","title":"Themis - Pius Omene | Services"},
-            "clients": {"js":"page-3.js","html":"page-3.html","title":"Themis - Pius Omene | Clients"},
-            "practice": {"js":"page-4.js","html":"page-4.html","title":"Themis - Pius Omene | Practice"},
-            "contact": {"js":"page-5.js","html":"page-5.html","title":"Themis - Pius Omene | Contact"}
-        };
-        var options = {
-            "openStart": "",
-            "title": "Themis - Pius Omene | Law Business Template",
-            "meta": {
-                "keywords": "advocate, attorney, law, business, corporate, court, finance, justice, lawyer, legal, minimal, minimalistic, office, themis",
-                "description": "Themis is a minimal corporate template related to law, business, investment etc. Home page is based on five vertical tabs with possibility to add unlimited number of additional tabs. Each tab is a single page. Included PSD source file allows you to customize this template to your needs."
-            },
-            "displayMenuSliderVertical": true,
-            "displayMenuSliderHorizontal": true
-        };
-        var request = '';
-
-        $(document).ready(function() {
-            $('#themis').themis(page, options, request);
-        });
-    </script>
 </body>
 </html>
